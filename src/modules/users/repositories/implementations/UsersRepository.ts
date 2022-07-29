289a13a391c94355d3aa56320ca54e5ad8379147
Matheus Santos <52337444+matheus-santos-moreira@users.noreply.github.com>
2022-07-29 14:47:39 +0000

    feat: finish test

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,24 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await  this.repository.findOneOrFail({
+      where: {
+        id: user_id
+      },
+      relations: ['games']
+    })
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users ORDER BY first_name`);
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users WHERE first_name ilike '%${first_name}%' AND last_name ilike '%${last_name}%'`);
   }
 }
